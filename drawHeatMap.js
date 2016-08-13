9874103fd1b745d88c1eb22a4ca73fe9f8853399
shakedt <dokuroc@gmail.com>
2016-08-13 12:22:07 +0300

    draw heatmap no functions correctly
    basic proof of concept

--- drawHeatMap.js
+++ drawHeatMap.js
@@ -1,3 +1,9 @@
 /**
  * Created by t on 8/13/2016.
  */
+//function to draw the heat map will move to its on page
+function drawHeatMap(clicks){
+    for (var i=0; i < clicks.length; i++){
+        $('body').append("<div style='position: absolute; left: " + clicks[i][0] + "px; top:" + clicks[i][1] + "px; width: 4px; height: 4px; background-color: red;'></div>");
+    };
+};
